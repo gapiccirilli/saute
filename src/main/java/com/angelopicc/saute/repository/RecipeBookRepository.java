0f5389555948771014d6056039f47b90ff228875
Gino Piccirilli <gapiccirilli@outlook.com>
2023-06-13 11:04:12 -0400

    create map helper methods and createRecipeBook()

--- src/main/java/com/angelopicc/saute/repository/RecipeBookRepository.java
+++ src/main/java/com/angelopicc/saute/repository/RecipeBookRepository.java
@@ -1,10 +1,12 @@
 package com.angelopicc.saute.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.angelopicc.saute.entity.RecipeBook;
 
 public interface RecipeBookRepository extends JpaRepository<RecipeBook, Long> {
     
-    RecipeBook findByRecipeBookName(String recipeBookName);
+    Optional<RecipeBook> findByRecipeBookName(String recipeBookName);
 }
--- src/main/java/com/angelopicc/saute/service/impl/StandardRecipeBookService.java
+++ src/main/java/com/angelopicc/saute/service/impl/StandardRecipeBookService.java
@@ -1,9 +1,12 @@
 package com.angelopicc.saute.service.impl;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
+import com.angelopicc.saute.entity.RecipeBook;
+import com.angelopicc.saute.exception.DuplicateNameException;
 import com.angelopicc.saute.payload.RecipeBookDto;
 import com.angelopicc.saute.repository.RecipeBookRepository;
 import com.angelopicc.saute.service.RecipeBookService;
@@ -19,7 +22,16 @@ public class StandardRecipeBookService implements RecipeBookService {
 
     @Override
     public RecipeBookDto createRecipeBook(RecipeBookDto recipeBook) {
-        return null;
+        // 1. check for duplicate recipe book names with helper method
+        if (hasNameDuplicate(recipeBook)) {
+            throw new DuplicateNameException("There is already a recipe book with the name \"" + recipeBook.getRecipeBookName() + "\"");
+        }
+        // 2. map to entity
+        RecipeBook recipeBookEntity = mapToEntity(recipeBook);
+        // 3. save recipebook
+        RecipeBook savedEntity = recipeBookRepository.save(recipeBookEntity);
+        // 4. map saved entity back to dto and return
+        return mapToDto(savedEntity);
     }
 
     @Override
@@ -52,4 +64,24 @@ public class StandardRecipeBookService implements RecipeBookService {
         throw new UnsupportedOperationException("Unimplemented method 'deleteRecipeBook'");
     }
     
+    private boolean hasNameDuplicate(RecipeBookDto recipeBook) {
+        Optional<RecipeBook> optRecipeBook = recipeBookRepository.findByRecipeBookName(recipeBook.getRecipeBookName());
+
+        if (optRecipeBook.isPresent()) {
+            return true;
+        }
+        return false;
+    }
+
+    private RecipeBook mapToEntity(RecipeBookDto dto) {
+        RecipeBook recipeBook = new RecipeBook();
+        recipeBook.setId(dto.getId());
+        recipeBook.setRecipeBookName(dto.getRecipeBookName());
+        return recipeBook;
+    }
+
+    private RecipeBookDto mapToDto(RecipeBook entity) {
+        RecipeBookDto dto = new RecipeBookDto(entity.getId(), entity.getRecipeBookName());
+        return dto;
+    }
 }
