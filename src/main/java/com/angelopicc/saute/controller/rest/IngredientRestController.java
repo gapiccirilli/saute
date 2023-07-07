dec1fafc851a7c9c5db0a21c0e1ff50e3f381df7
Gino Piccirilli <gapiccirilli@outlook.com>
2023-07-07 16:40:47 -0400

    add cross origin for development with react

--- src/main/java/com/angelopicc/saute/controller/rest/IngredientRestController.java
+++ src/main/java/com/angelopicc/saute/controller/rest/IngredientRestController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,7 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.angelopicc.saute.payload.IngredientDto;
 import com.angelopicc.saute.service.IngredientService;
 
-
+@CrossOrigin
 @RestController
 @RequestMapping("/api/ingredients")
 public class IngredientRestController {
